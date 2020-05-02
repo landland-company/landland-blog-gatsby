833ee4a2c49ce4f152e42cc3d915c0b1a32e7883
WataruFujiwara <wataru930@gmail.com>
2020-05-02 19:59:08 +0900

    バーの名前の変更

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,16 +60,16 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                事業内容
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                実績
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                お問い合わせ
               </Link>
             </div>
           </div>
