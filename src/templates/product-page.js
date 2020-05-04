fa7c182071beb94a190970a924120c98935ae829
WataruFujiwara <wataru930@gmail.com>
2020-05-04 14:56:13 +0900

    画像固定表示

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -19,9 +19,7 @@ export const ProductPageTemplate = ({
     <div
       className="full-width-image-container margin-top-0"
       style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
+        backgroundImage: `url(img/landland_homepage.gif)`,
       }}
     >
       <h2
